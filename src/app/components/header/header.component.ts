eadbd4c009814a1b0b94c82d76accc9de0601419
Michał Skoczek <mwskoczek@gmail.com>
2023-01-17 23:24:16 +0100

    feat: header component implemented

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryModel } from '../../models/category.model';
+import { CategoryService } from '../../services/category.service';
+
+@Component({
+  selector: 'app-header',
+  styleUrls: ['./header.component.scss'],
+  templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+  readonly categories$: Observable<CategoryModel[]> = this._categoryService.getCategories();
+
+  constructor(private _categoryService: CategoryService) {
+  }
+}
