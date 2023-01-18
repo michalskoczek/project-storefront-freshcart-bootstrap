c60ab2c29e342fa2f065b07f2a54d1b086d91797
Michał Skoczek <mwskoczek@gmail.com>
2023-01-18 19:30:49 +0100

    feat: added handling mobile menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Observable, of, shareReplay} from "rxjs";
+import {CategoryModel} from "../../models/category.model";
+import {CategoryService} from "../../services/category.service";
+import {StoreModel} from "../../models/store.model";
+import {StoreService} from "../../services/store.service";
 
 @Component({
   selector: 'app-home',
@@ -8,4 +13,20 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
+
+  readonly categories$: Observable<CategoryModel[]> = this._categoryService.getCategories().pipe(shareReplay(1));
+  readonly stores$: Observable<StoreModel[]> = this._storeService.getStories();
+  readonly getToKnowsUsElements$: Observable<string[]> = of(['Company', 'About', 'Blog', 'Help Center', 'Our Value']);
+
+  constructor(private _categoryService: CategoryService, private _storeService: StoreService) {
+  }
+
+  public openMobileMenu(): void {
+    this.mobileMenu.nativeElement.style = 'transform: translateX(0); visibility: visible';
+  }
+
+  public onCloseMobileMenu(): void {
+    this.mobileMenu.nativeElement.style = '';
+  }
 }
