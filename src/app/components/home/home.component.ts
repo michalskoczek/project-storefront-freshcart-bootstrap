2e3fb9d7d505fe2f63656805c0c472d36615e998
Michał Skoczek <mwskoczek@gmail.com>
2023-01-19 21:23:24 +0100

    fixed menu mobile display

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
-import {ChangeDetectionStrategy, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
-import {Observable, of, shareReplay} from "rxjs";
-import {CategoryModel} from "../../models/category.model";
-import {CategoryService} from "../../services/category.service";
-import {StoreModel} from "../../models/store.model";
-import {StoreService} from "../../services/store.service";
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable, Subject, of, shareReplay } from 'rxjs';
+import { CategoryModel } from '../../models/category.model';
+import { StoreModel } from '../../models/store.model';
+import { CategoryService } from '../../services/category.service';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-home',
@@ -13,20 +13,20 @@ import {StoreService} from "../../services/store.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
-
   readonly categories$: Observable<CategoryModel[]> = this._categoryService.getCategories().pipe(shareReplay(1));
   readonly stores$: Observable<StoreModel[]> = this._storeService.getStories();
   readonly getToKnowsUsElements$: Observable<string[]> = of(['Company', 'About', 'Blog', 'Help Center', 'Our Value']);
+  private _showMobileMenuSubject: Subject<boolean> = new Subject<boolean>();
+  public showMobileMenu$: Observable<boolean> = this._showMobileMenuSubject.asObservable();
 
   constructor(private _categoryService: CategoryService, private _storeService: StoreService) {
   }
 
-  public openMobileMenu(): void {
-    this.mobileMenu.nativeElement.style = 'transform: translateX(0); visibility: visible';
+  public showMobileMenu(): void {
+    this._showMobileMenuSubject.next(true)
   }
 
-  public onCloseMobileMenu(): void {
-    this.mobileMenu.nativeElement.style = '';
+  public hideMobileMenu(): void {
+    this._showMobileMenuSubject.next(false)
   }
 }
