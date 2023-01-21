1510eb215ecec9a4b36c62a2f2c9b1a0bd75a901
Michał Skoczek <mwskoczek@gmail.com>
2023-01-21 17:37:25 +0100

    feat: distance in meters to kilometers changed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable, Subject, of, shareReplay } from 'rxjs';
-import { CategoryModel } from '../../models/category.model';
-import { StoreModel } from '../../models/store.model';
-import { CategoryService } from '../../services/category.service';
-import { StoreService } from '../../services/store.service';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {Observable, Subject, of, shareReplay, map, tap} from 'rxjs';
+import {CategoryModel} from '../../models/category.model';
+import {StoreModel} from '../../models/store.model';
+import {CategoryService} from '../../services/category.service';
+import {StoreService} from '../../services/store.service';
 
 @Component({
   selector: 'app-home',
@@ -14,10 +14,15 @@ import { StoreService } from '../../services/store.service';
 })
 export class HomeComponent {
   readonly categories$: Observable<CategoryModel[]> = this._categoryService.getCategories().pipe(shareReplay(1));
-  readonly stores$: Observable<StoreModel[]> = this._storeService.getStories();
   readonly getToKnowsUsElements$: Observable<string[]> = of(['Company', 'About', 'Blog', 'Help Center', 'Our Value']);
   private _showMobileMenuSubject: Subject<boolean> = new Subject<boolean>();
   public showMobileMenu$: Observable<boolean> = this._showMobileMenuSubject.asObservable();
+
+  readonly stores$: Observable<StoreModel[]> = this._storeService.getStories().pipe(
+    shareReplay(1),
+    map(store => store.map(store => ({...store, distanceInMeters: +((store.distanceInMeters / 1000).toFixed(1))})
+    ))
+  );
 
   constructor(private _categoryService: CategoryService, private _storeService: StoreService) {
   }
